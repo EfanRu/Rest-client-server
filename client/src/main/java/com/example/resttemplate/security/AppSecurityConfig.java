116ce90ce849a5bbea92bb209a8bc7b2a10f57ab
Vyacheslav <ffgeniy@mail.ru>
2020-03-12 22:53:08 +0300

    Some changes to OAuth2 authentication

--- client/src/main/java/com/example/resttemplate/security/AppSecurityConfig.java
+++ client/src/main/java/com/example/resttemplate/security/AppSecurityConfig.java
@@ -2,10 +2,10 @@ package com.example.resttemplate.security;
 
 import com.example.resttemplate.service.UserServiceRest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,60 +16,57 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
-//@EnableOAuth2Client
+@EnableOAuth2Client
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserServiceRest userService;
 
-//    private final OAuth2ClientContext oAuth2ClientContext;
+    private final OAuth2ClientContext oAuth2ClientContext;
 
     @Autowired
-    public AppSecurityConfig(UserServiceRest userService/*, OAuth2ClientContext oAuth2ClientContext*/) {
+    public AppSecurityConfig(UserServiceRest userService, OAuth2ClientContext oAuth2ClientContext) {
         this.userService = userService;
-//        this.oAuth2ClientContext = oAuth2ClientContext;
+        this.oAuth2ClientContext = oAuth2ClientContext;
     }
-//
-//    @Bean
-//    public FilterRegistrationBean oAuth2ClientFilterRegistration(OAuth2ClientContextFilter oAuth2ClientContextFilter)
-//    {
-//        FilterRegistrationBean registration = new FilterRegistrationBean();
-//        registration.setFilter(oAuth2ClientContextFilter);
-//        registration.setOrder(-100);
-//        return registration;
-//    }
-//
-//    private ComponentScan.Filter ssoFilter()
-//    {
-//        OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/google");
-//        OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oAuth2ClientContext);
-//        googleFilter.setRestTemplate(googleTemplate);
-//        CustomUserInfoTokenServices tokenServices = new CustomUserInfoTokenServices(googleResource().getUserInfoUri(), google().getClientId());
-//        tokenServices.setRestTemplate(googleTemplate);
-//        googleFilter.setTokenServices(tokenServices);
-//        tokenServices.setUserRepo(userRepo);
-//        tokenServices.setPasswordEncoder(passwordEncoder);
-//        return googleFilter;
-//    }
-//
-//    @Bean
-//    @ConfigurationProperties("google.client")
-//    public AuthorizationCodeResourceDetails google()
-//    {
-//        return new AuthorizationCodeResourceDetails();
-//    }
-//
-//    @Bean
-//    @ConfigurationProperties("google.resource")
-//    public ResourceServerProperties googleResource()
-//    {
-//        return new ResourceServerProperties();
-//    }
+
+    @Bean
+    public FilterRegistrationBean oAuth2ClientFilterRegistration(OAuth2ClientContextFilter oAuth2ClientContextFilter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(oAuth2ClientContextFilter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/google");
+        OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oAuth2ClientContext);
+        googleFilter.setRestTemplate(googleTemplate);
+        CustomUserInfoTokenService tokenServices = new CustomUserInfoTokenService(googleResource().getUserInfoUri(), google().getClientId());
+        tokenServices.setRestTemplate(googleTemplate);
+        googleFilter.setTokenServices(tokenServices);
+        return googleFilter;
+    }
+
+    @Bean
+    @ConfigurationProperties("google.client")
+    public AuthorizationCodeResourceDetails google() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.resource")
+    public ResourceServerProperties googleResource() {
+        return new ResourceServerProperties();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -95,7 +92,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
-//                .addFilterBefore(ssoFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(ssoFilter(), UsernamePasswordAuthenticationFilter.class)
             .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").authenticated()
